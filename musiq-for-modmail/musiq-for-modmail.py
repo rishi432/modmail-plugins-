b9a4e9f63991900e8c6f5efba7e9187ee8a449d8
kuro <46526001+WebKide@users.noreply.github.com>
2020-08-10 21:47:43 +0100

    tidying up

--- musiq-for-modmail/musiq-for-modmail.py
+++ musiq-for-modmail/musiq-for-modmail.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
+# okay, there are a lot of things missing here, I'm trying to get this to work...
 
 import discord, functools, itertools, math, random, youtube_dl, asyncio
 
 from discord.ext import commands
 from async_timeout import timeout
 
-# Silence useless bug reports messages
+# Silences all the useless bug-report messages
 youtube_dl.utils.bug_reports_message = lambda: ''
 
 
@@ -250,7 +251,13 @@ class VoiceState:
             self.voice = None
 
 
+# +------------------------------------------------------------+
+# |              MUSIC PLUGIN COMMANDS                         |
+# +------------------------------------------------------------+
 class Musiq(commands.Cog):
+    """(∩｀-´)⊃━☆ﾟ.*･｡ﾟ Musiq plugin for Modmail
+    You need to be in a voice channel to use these cmds!
+    """
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.voice_states = {}
@@ -281,8 +288,7 @@ class Musiq(commands.Cog):
 
     @commands.command(name='join', invoke_without_subcommand=True)
     async def _join(self, ctx: commands.Context):
-        """Joins a voice channel."""
-
+        """Joins a voice channel with you."""
         destination = ctx.author.voice.channel
         if ctx.voice_state.voice:
             await ctx.voice_state.voice.move_to(destination)
@@ -297,7 +303,6 @@ class Musiq(commands.Cog):
 
         If no channel was specified, it joins your channel.
         """
-
         if not channel and not ctx.author.voice:
             raise VoiceError('You are neither connected to a voice channel nor specified a channel to join.')
 
@@ -312,7 +317,6 @@ class Musiq(commands.Cog):
     @commands.has_permissions(manage_guild=True)
     async def _leave(self, ctx: commands.Context):
         """Clears the queue and leaves the voice channel."""
-
         if not ctx.voice_state.voice:
             return await ctx.send('Not connected to any voice channel.')
 
@@ -322,7 +326,6 @@ class Musiq(commands.Cog):
     @commands.command(name='volume')
     async def _volume(self, ctx: commands.Context, *, volume: int):
         """Sets the volume of the player."""
-
         if not ctx.voice_state.is_playing:
             return await ctx.send('Nothing being played at the moment.')
 
@@ -332,17 +335,15 @@ class Musiq(commands.Cog):
         ctx.voice_state.volume = volume / 100
         await ctx.send('Volume of the player set to {}%'.format(volume))
 
-    @commands.command(name='now', aliases=['current', 'playing'])
+    @commands.command(name='now', aliases=['current', 'playing', 'np'])
     async def _now(self, ctx: commands.Context):
         """Displays the currently playing song."""
-
         await ctx.send(embed=ctx.voice_state.current.create_embed())
 
     @commands.command(name='pause')
     @commands.has_permissions(manage_guild=True)
     async def _pause(self, ctx: commands.Context):
         """Pauses the currently playing song."""
-
         if not ctx.voice_state.is_playing and ctx.voice_state.voice.is_playing():
             ctx.voice_state.voice.pause()
             await ctx.message.add_reaction('⏯')
@@ -351,7 +352,6 @@ class Musiq(commands.Cog):
     @commands.has_permissions(manage_guild=True)
     async def _resume(self, ctx: commands.Context):
         """Resumes a currently paused song."""
-
         if not ctx.voice_state.is_playing and ctx.voice_state.voice.is_paused():
             ctx.voice_state.voice.resume()
             await ctx.message.add_reaction('⏯')
@@ -360,7 +360,6 @@ class Musiq(commands.Cog):
     @commands.has_permissions(manage_guild=True)
     async def _stop(self, ctx: commands.Context):
         """Stops playing song and clears the queue."""
-
         ctx.voice_state.songs.clear()
 
         if not ctx.voice_state.is_playing:
@@ -369,8 +368,7 @@ class Musiq(commands.Cog):
 
     @commands.command(name='skip')
     async def _skip(self, ctx: commands.Context):
-
-
+        """Skip to the next song in the queue."""
         if not ctx.voice_state.is_playing:
             return await ctx.send('Not playing any music right now...')
 
@@ -392,13 +390,12 @@ class Musiq(commands.Cog):
         else:
             await ctx.send('You have already voted to skip this song.')
 
-    @commands.command(name='queue')
+    @commands.command(name='queue', aliases=['q'])
     async def _queue(self, ctx: commands.Context, *, page: int = 1):
-        """Shows the player's queue.
+        """Shows the current queue.
 
         You can optionally specify the page to show. Each page contains 10 elements.
         """
-
         if len(ctx.voice_state.songs) == 0:
             return await ctx.send('Empty queue.')
 
@@ -419,7 +416,6 @@ class Musiq(commands.Cog):
     @commands.command(name='shuffle')
     async def _shuffle(self, ctx: commands.Context):
         """Shuffles the queue."""
-
         if len(ctx.voice_state.songs) == 0:
             return await ctx.send('Empty queue.')
 
@@ -429,7 +425,6 @@ class Musiq(commands.Cog):
     @commands.command(name='remove')
     async def _remove(self, ctx: commands.Context, index: int):
         """Removes a song from the queue at a given index."""
-
         if len(ctx.voice_state.songs) == 0:
             return await ctx.send('Empty queue.')
 
@@ -442,7 +437,6 @@ class Musiq(commands.Cog):
 
         Invoke this command again to unloop the song.
         """
-
         if not ctx.voice_state.is_playing:
             return await ctx.send('Nothing being played at the moment.')
 
@@ -460,7 +454,6 @@ class Musiq(commands.Cog):
         This command automatically searches from various sites if no URL is provided.
         A list of these sites can be found here: https://rg3.github.io/youtube-dl/supportedsites.html
         """
-
         if not ctx.voice_state.voice:
             await ctx.invoke(self._join)
 
